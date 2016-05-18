406778b70e3aaeba2ac91d84e2ae1e663a6f3a67
Sharipkin_D <Sharipkin_D@Aktobe.Railways.kz>
2016-05-18 14:49:26 +0600

    LocalBean Napr

---
+++ EJB_ProASUClient/ejbModule/kz/railways/entities/Napr.java
@@ -0,0 +1,25 @@
+package kz.railways.entities;
+
+import java.io.Serializable;
+
+public class Napr implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int kod;
+	private String naim;
+	
+	public int getKod() {
+		return kod;
+	}
+	public void setKod(int kod) {
+		this.kod = kod;
+	}
+	public String getNaim() {
+		return naim;
+	}
+	public void setNaim(String naim) {
+		this.naim = naim;
+	}
+
+}
