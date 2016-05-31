e7e866dca45fe26f05ee32d7f89f52d26c875d83
Sharipkin_D <Sharipkin_D@Aktobe.Railways.kz>
2016-05-31 15:55:39 +0600

    Исправлен клиент для работы по сети

--- ClientImport/appClientModule/main/MessageSender.java
+++ ClientImport/appClientModule/main/MessageSender.java
@@ -9,28 +9,28 @@ import java.nio.file.Paths;
 import java.util.Date;
 import java.util.Scanner;
 
-import javax.annotation.Resource;
 import javax.jms.BytesMessage;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
+import javax.jms.QueueConnection;
 import javax.jms.Session;
+import com.sun.messaging.ConnectionConfiguration;
+import com.sun.messaging.ConnectionFactory;
 
 
 
 public class MessageSender {
 	
-	@Resource(mappedName = "jms/ImportConnFactory")
-	private static ConnectionFactory connFact;
-	@Resource(mappedName = "jms/ImportQueue")
-	private static Queue queue;
+	//@Resource(mappedName = "jms/ImportConnFactory")
+	//private static ConnectionFactory connFact;
+	//@Resource(mappedName = "jms/ImportQueue")
+	//private static Queue queue;
 	
 	private static Scanner sc;
 	
 	
-	public void produceMessage(byte [] mess)
+	/*public void produceMessage(byte [] mess)
 	{
 		BytesMessage bm;
 		
@@ -52,10 +52,71 @@ public class MessageSender {
 		} catch (JMSException e) {
 			e.printStackTrace();
 		}
+	}*/
+	
+	public void produceMessage(byte [] mess)
+	{
+		BytesMessage bm;
+		
+		Session session=null;
+		ConnectionFactory factory=null;
+		QueueConnection connection=null;
+		
+		try
+		{
+			//Properties props = new Properties();
+
+            //props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+            //props.setProperty("org.omg.CORBA.ORBInitialHost", "10.64.3.80");
+            //props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+			
+		     //Context jndiContext = new InitialContext(props);
+		     // Look up the factory and the queue
+		     //factory = (ConnectionFactory) jndiContext.lookup("jms/ImportConnFactory");
+			
+			 factory = new com.sun.messaging.ConnectionFactory();  
+		     factory.setProperty(ConnectionConfiguration.imqAddressList, "mq://10.64.3.80:7676,mq://10.64.3.80:7676");
+		     connection = factory.createQueueConnection();
+			
+		     connection.start();
+
+		      session = connection.createQueueSession( 
+		                          false, Session.AUTO_ACKNOWLEDGE);
+		      Queue ioQueue = session.createQueue("ImportQueue");
+		      MessageProducer queueSender = session.createProducer(ioQueue);
+			
+			bm = session.createBytesMessage();
+			bm.writeBytes(mess);
+			
+			
+			
+			//tm.setText("Test Message" + new Date().toString());
+			System.out.println("Send Message ---- "+new Date().toString()+bm.getClass());
+			String content = new String(mess, StandardCharsets.UTF_8);
+			System.out.println(content);
+			
+			queueSender.send(bm);
+		    queueSender.close();
+
+			
+		}
+	    catch (JMSException e){
+	           System.out.println("Error: " + e.getMessage()); }
+	    finally{
+	       try{
+	          session.close();
+	          connection.close();
+	       } catch (Exception e) {
+	          System.out.println("Can’t close JMS connection/session " + e.getMessage());
+	      }
+	    }	
 	}
+	
 
 	public static void main(String[] args) throws JMSException{
 		
+		System.setProperty("org.omg.CORBA.ORBInitialHost", "10.64.3.80");
+	    System.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
 		
     	Path dir = Paths.get("d:\\Programming\\Java\\ki");
     	sc = new Scanner(System.in);
@@ -79,6 +140,7 @@ public class MessageSender {
 	        		{
 	        			System.out.println(entry.getFileName());
 	        			
+	        			//new MessageSender().produceMessage(Files.readAllBytes(entry));
 	        			new MessageSender().produceMessage(Files.readAllBytes(entry));
 	        		}
 	        		
