82670137ce5267905d2a4db1d61c57b3acb86f2d
Sharipkin_D <Sharipkin_D@Aktobe.Railways.kz>
2016-06-11 12:20:28 +0600

    Добавлен бин парки, доработка подход бина

--- EJB_ProASU/ejbModule/kz/railways/workstation/ParkiBean.java
+++ EJB_ProASU/ejbModule/kz/railways/workstation/ParkiBean.java
@@ -31,16 +31,15 @@ public class ParkiBean implements ParkiBeanLocal {
      * Default constructor. 
      */
 	@Override
-	public List<Park> getPOPark(String kodst, int s){
-		String sql = "SELECT * FROM STATION_PARK WHERE TIP_PARK <> 4 AND TIP_PARK<>2 AND KOD_S = ? AND KOD_ST = ? ORDER BY NPP, N_PARK";
+	public List<Park> getPOPark(String kodst){
+		String sql = "SELECT * FROM STATION_PARK WHERE TIP_PARK <> 4 AND TIP_PARK<>2 AND KOD_ST = ? ORDER BY NPP, N_PARK";
 		
 		List<Park> lp = new ArrayList<>();
 		
 		try (Connection conn = dataSource.getConnection();
 			PreparedStatement ps = conn.prepareStatement(sql);)	
-		{
-			ps.setInt(1, s);
-			ps.setString(2, kodst);
+		{			
+			ps.setString(1, kodst);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next())
 			{
@@ -49,47 +48,8 @@ public class ParkiBean implements ParkiBeanLocal {
 				park.setName(rs.getString("NAIM_PARK"));
 				park.setKodS(rs.getInt("KOD_S"));
 				
-				List<Put> lpPut = new ArrayList<>();
-				String sqlPut = "SELECT STATION_PARKPUT.KOD_ST, STATION_PARKPUT.N_PARK,STATION_PARKPUT.N_PUT,STATION_PARKPUT.MNKD_PUT,STATION_PARKPUT.NAIM_PUT,"+
-				        "P_POP.IND_POEZD,P_POP.N_PARK,P_POP.N_PUT,P_POP.ST_NAZN,P_POP.N_SOST, "+
-				        "P_POP.ST_FORM,P_POP.N_POEZD,P_POP.MNKD_OP,P_POP.PR_TR,P_POP.S_TGNL, "+
-				        "P_POP.PR_RASF, P_POP.DV_OPER, "+
-				        "P_POP.HAR_P,P_POP.KOL_OS,P_POP.KOL_ROL,P_POP.KOD_ST, "+
-				        "P_TEK.IND_POEZD,P_TEK.BRUTTO,P_TEK.UDL,P_TEK.NEGAB, "+
-				        "P_TEK.KOL_VAG,P_TEK.NVAG_N,P_TEK.NVAG_K,P_TEK.PR_OHR,P_TEK.PRIK,P_TEK.KOD_OP "+
-				        "FROM STATION_PARKPUT LEFT JOIN P_POP "+
-				             "ON STATION_PARKPUT.N_PARK = P_POP.N_PARK AND "+ 
-				                   "STATION_PARKPUT.N_PUT = P_POP.N_PUT AND STATION_PARKPUT.KOD_ST = P_POP.KOD_ST "+
-				                "LEFT JOIN P_TEK ON P_POP.IND_POEZD = P_TEK.IND_POEZD and P_POP.KOD_ST=P_TEK.KOD_ST "+
-				                "WHERE STATION_PARKPUT.N_PARK = ? AND STATION_PARKPUT.KOD_ST = ? ORDER BY STATION_PARKPUT.N_PUT";
-				try(PreparedStatement psPut = conn.prepareStatement(sqlPut)){
-					psPut.setInt(1, park.getnPark());
-					psPut.setString(2, kodst);
-					
-					ResultSet rsPut = psPut.executeQuery();
-					while(rsPut.next()){
-						Put put = new Put();
-						put.setnPark(rsPut.getInt("N_PARK"));
-						put.setnPut(rsPut.getInt("N_PUT"));
-						put.setNaimPut(rsPut.getString("NAIM_PUT"));
-						
-						Poezd poezd = new Poezd();
-						poezd.setnPoezd(rsPut.getString("N_POEZD"));
-						poezd.setStForm(rsPut.getString("ST_FORM"));
-						poezd.setnSost(rsPut.getString("N_SOST"));
-						poezd.setStNazn(rsPut.getString("ST_NAZN"));
-						poezd.setKolVag(rsPut.getInt("KOL_VAG"));
-								
-						put.setPoezd(poezd);
-						lpPut.add(put);
-					}
-					
-				}
-				catch (SQLException e) 
-				{
-					e.printStackTrace();
-					return null;
-				}
+				List<Put> lpPut = getPuti(kodst, park.getnPark());
+				
 				park.setPutList(lpPut);
 				lp.add(park);
 				
@@ -104,6 +64,69 @@ public class ParkiBean implements ParkiBeanLocal {
 		return lp;
 	}
 	
+	@Override
+	public List<Put> getPuti(String kodst, int park){
+		List<Put> lpPut = new ArrayList<>();
+		String sqlPut = "SELECT STATION_PARKPUT.KOD_ST, STATION_PARKPUT.N_PARK,STATION_PARKPUT.N_PUT,STATION_PARKPUT.MNKD_PUT,STATION_PARKPUT.NAIM_PUT,"+
+		        "P_POP.IND_POEZD,P_POP.N_PARK,P_POP.N_PUT,P_POP.ST_NAZN,P_POP.N_SOST, "+
+		        "P_POP.ST_FORM,P_POP.N_POEZD,P_POP.MNKD_OP,P_POP.PR_TR,P_POP.S_TGNL, "+
+		        "P_POP.PR_RASF, P_POP.DV_OPER, "+
+		        "P_POP.HAR_P,P_POP.KOL_OS,P_POP.KOL_ROL,P_POP.KOD_ST,P_POP.RAB_LOK, "+
+		        "P_TEK.IND_POEZD,P_TEK.BRUTTO,P_TEK.UDL,P_TEK.NEGAB, P_TEK.GIVN, "+
+		        "P_TEK.KOL_VAG,P_TEK.NVAG_N,P_TEK.NVAG_K,P_TEK.PR_OHR,P_TEK.PRIK,P_TEK.KOD_OP,P_TEK.MARSH "+
+		        "FROM STATION_PARKPUT LEFT JOIN P_POP "+
+		             "ON STATION_PARKPUT.N_PARK = P_POP.N_PARK AND "+ 
+		                   "STATION_PARKPUT.N_PUT = P_POP.N_PUT AND STATION_PARKPUT.KOD_ST = P_POP.KOD_ST "+
+		                "LEFT JOIN P_TEK ON P_POP.IND_POEZD = P_TEK.IND_POEZD and P_POP.KOD_ST=P_TEK.KOD_ST "+
+		                "WHERE STATION_PARKPUT.N_PARK = ? AND STATION_PARKPUT.KOD_ST = ? ORDER BY STATION_PARKPUT.N_PUT";
+		try(Connection conn = dataSource.getConnection();
+			PreparedStatement psPut = conn.prepareStatement(sqlPut)){
+			psPut.setInt(1, park);
+			psPut.setString(2, kodst);
+			
+			ResultSet rsPut = psPut.executeQuery();
+			while(rsPut.next()){
+				Put put = new Put();
+				put.setnPark(rsPut.getInt("N_PARK"));
+				put.setnPut(rsPut.getInt("N_PUT"));
+				put.setNaimPut(rsPut.getString("NAIM_PUT"));
+				
+				Poezd poezd = new Poezd();
+				poezd.setIndPoezd(rsPut.getString("IND_POEZD"));
+				poezd.setnPoezd(rsPut.getString("N_POEZD"));
+				poezd.setStForm(rsPut.getString("ST_FORM"));
+				poezd.setnSost(rsPut.getString("N_SOST"));
+				poezd.setStNazn(rsPut.getString("ST_NAZN"));
+				poezd.setMnkdOp(rsPut.getString("MNKD_OP"));				
+				poezd.setDvOper(rsPut.getTimestamp("DV_OPER"));
+				poezd.setKolVag(rsPut.getInt("KOL_VAG"));
+				poezd.setBrutto(rsPut.getInt("BRUTTO"));
+				poezd.setUdl(rsPut.getInt("UDL"));
+				poezd.setKolOs(rsPut.getInt("KOL_OS"));
+				poezd.setKolRol(rsPut.getInt("KOL_ROL"));
+				poezd.setsTgnl(rsPut.getString("S_TGNL"));
+				poezd.setHarP(rsPut.getString("HAR_P"));
+				poezd.setNegab(rsPut.getString("NEGAB"));
+				poezd.setGivn(rsPut.getInt("GIVN"));
+				poezd.setPrik(rsPut.getInt("PRIK"));
+				poezd.setPrOhr(rsPut.getInt("PR_OHR"));
+				poezd.setNvagK(rsPut.getString("NVAG_N"));
+				poezd.setNvagN(rsPut.getString("NVAG_K"));
+				poezd.setRabLok(rsPut.getInt("RAB_LOK"));
+						
+				put.setPoezd(poezd);
+				lpPut.add(put);
+			}
+			
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return null;
+		}
+		
+		return lpPut;
+	}
 	@Override
 	public List<Put> getSortPark(String kodst){
 		String sql = "SELECT * FROM STATION_PARKPUT where N_PARK = 3 and KOD_ST = ?";
@@ -130,5 +153,45 @@ public class ParkiBean implements ParkiBeanLocal {
 		
 		return lp;		
 	}
-
+	
+	@Override
+	public Boolean hasRecPTek(String indP){
+		String sql = "SELECT count(*) as rowCount FROM P_TEK WHERE IND_POEZD = ?";
+		
+		try (Connection conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);)
+		{
+			ps.setString(1, indP);
+			ResultSet rs = ps.executeQuery();
+			if (rs.getInt("rowCount")==0)
+				return false;
+			else
+				return true;			
+		}catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}		
+	}
+	
+	
+	@Override
+	public Boolean hasRecPPOP(String park, String put, String kodst){
+		String sql = "SELECT * FROM P_POP WHERE KOD_ST = ? AND N_PARK = ? AND N_PUT = ?";
+		
+		try (Connection conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);)
+		{
+			ps.setString(1, kodst);
+			ps.setString(2, park);
+			ps.setString(3, put);
+			ResultSet rs = ps.executeQuery();
+			if (rs.getString("IND_POEZD").equals(""))
+				return false;
+			else
+				return true;			
+		}catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
--- EJB_ProASU/ejbModule/kz/railways/workstation/PodhodBean.java
+++ EJB_ProASU/ejbModule/kz/railways/workstation/PodhodBean.java
@@ -69,7 +69,7 @@ public class PodhodBean implements PodhodBeanLocal {
 				poezd.setUdl(rs.getInt("UDL"));
 				poezd.setBrutto(rs.getInt("BRUTTO"));
 				poezd.setPrik(rs.getInt("PRIK"));
-				poezd.setNegab(rs.getInt("NEGAB"));
+				poezd.setNegab(rs.getString("NEGAB"));
 				poezd.setGivn(rs.getInt("GIVN"));
 				poezd.setMarsh(rs.getString("MARSH"));
 				poezd.setKolVag(rs.getInt("KOL_VAG"));
@@ -160,12 +160,14 @@ public class PodhodBean implements PodhodBeanLocal {
 	public void add(Poezd poezd) {
 		
 		String sql = "insert into P_TEK(IND_POEZD, GIVN, ST_PER, ST_FORM, N_SOST, ST_NAZN, PR_SPIS, "
-				+ "MARSH, UDL, BRUTTO, NETTO, KOL_VAG, NVAG_N, NVAG_K, PR_OHR, KOD_ST, KOD_OP, DV_OTPR, DV_OPER, N_POEZD) "
-				+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				+ "MARSH, UDL, BRUTTO, NETTO, KOL_VAG, NVAG_N, NVAG_K, PR_OHR, KOD_ST, KOD_OP, DV_OTPR, DV_OPER, N_POEZD, "
+				+ "HAR_P, NBE) "
+				+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		try(Connection conn = dataSource.getConnection();)
 		{
-			try(PreparedStatement ps = conn.prepareStatement(sql);)
-			{			
+			try
+			{	
+				PreparedStatement ps = conn.prepareStatement(sql);
 				
 				ps.setString(1, poezd.getIndPoezd());
 				ps.setInt(2, poezd.getGivn());
@@ -187,19 +189,16 @@ public class PodhodBean implements PodhodBeanLocal {
 				ps.setTimestamp(18, poezd.getDvOtpr());
 				ps.setTimestamp(19, poezd.getDvOper());
 				ps.setString(20, poezd.getnPoezd());
+				ps.setString(21, poezd.getHarP());
+				ps.setString(22, poezd.getNbe());
 				ps.executeUpdate();
 				ps.close();
 				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 			
-			sql = "insert into P_VAG(IND_POEZD, NPP, NVAG, QUAL, KOD_SOB, ROLIK, VESGR, ST_NAZNV, "
-					+ "GRPOL, MARSH, PRIK, GIVN, KOL_PL, GR_KONT, POR_KONT, ESR_VP, TARA_UT, PRIM) "
-					+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-			
-			try(PreparedStatement ps = conn.prepareStatement(sql);)
-			{	
+				sql = "insert into P_VAG(IND_POEZD, NPP, NVAG, QUAL, KOD_SOB, ROLIK, VESGR, ST_NAZNV, "
+					+ "GRPOL, MARSH, PRIK, GIVN, KOL_PL, GR_KONT, POR_KONT, ESR_VP, TARA_UT, PRIM, KODGR) "
+					+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				ps = conn.prepareStatement(sql);
 				for (Vagon v : poezd.getVagonList()){
 					ps.setString(1, v.getIndPoezd());
 					ps.setInt(2, v.getNpp());
@@ -217,9 +216,9 @@ public class PodhodBean implements PodhodBeanLocal {
 					ps.setInt(14, v.getGrKont());
 					ps.setInt(15, v.getPorKont());
 					ps.setString(16, v.getEsrVp());
-					ps.setDouble(17, v.getTara());
+					ps.setDouble(17, v.getTaraUt());
 					ps.setString(18, v.getPrim());
-					
+					ps.setString(19, v.getKodGr());
 					ps.executeUpdate();
 					/*System.out.printf("NPP = %d, NVAG = %s, QUAL = %s, KOD_SOB = %s, ROLIK = %s, "
 					+ "VESGR = %d, ST_NAZN = %s, GR_POL = %s, MARSH = %s, PRIK = %s, GIVN = %s, "
@@ -227,23 +226,27 @@ public class PodhodBean implements PodhodBeanLocal {
 					v.getQual(), v.getKodSob(), v.getRolik(), v.getVesGr(), 
 					v.getStNazn(), v.getGrPol(), v.getMarsh(), v.getPrik(),
 					v.getPrik(), v.getKolPl(), v.getGrKont(), v.getPorKont(), v.getEsrVp(), v.getPrim());*/
-				}			
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+				}	
+				ps.close();
 			
-			sql = "call WEBUSER.ZAP_SFRAZA(?,?,?,?)";
-			
-			try(PreparedStatement ps = conn.prepareStatement(sql);)
-			{	
+				sql = "call WEBUSER.ZAP_SFRAZA(?,?,?,?)";
+				ps = conn.prepareStatement(sql);
 				ps.setString(1, poezd.getIndPoezd());
 				ps.setTimestamp(2, poezd.getDvOper());
 				ps.setInt(3, poezd.getKodOp());
 				ps.setInt(4, poezd.getPrDost());
 					
-				ps.executeUpdate();		
+				ps.executeUpdate();	
+				ps.close();
+				
 			} catch (SQLException e) {
 				e.printStackTrace();
+				try{
+					 if(conn!=null)
+			            conn.rollback();
+			       }catch(SQLException se2){
+			    	   se2.printStackTrace();
+			       }
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
